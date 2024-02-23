e772d677326bad99be00d9ba113129ba430fb416
mayukhdeb <mayukhmainak2000@gmail.com>
2024-02-23 19:31:16 +0530

    another indent oopsie

--- papyrus/style_css.py
+++ papyrus/style_css.py
@@ -18,45 +18,43 @@ class StyleCSS:
         import_color: str = "#d19a66",
         function_color: str = "#61afef",
     ):
-        self.css =  f"""
-    /* latex-style.css */
-    body {{
-        font-family: "{font_family}";
-        line-height: {line_height};
-        margin: {margin_top} {margin_left_right}; /* Keep the top and bottom margin as {margin_top} and set left and right margin to {margin_left_right} */
-        background-color: {background_color};
-        max-width: {max_width}px; /* Set your desired maximum width */
-    }}
-    .title {{
-        text-align: {title_text_align};
-    }}
+        self.css =  f"""/* latex-style.css */
+body {{
+    font-family: "{font_family}";
+    line-height: {line_height};
+    margin: {margin_top} {margin_left_right}; /* Keep the top and bottom margin as {margin_top} and set left and right margin to {margin_left_right} */
+    background-color: {background_color};
+    max-width: {max_width}px; /* Set your desired maximum width */
+}}
+.title {{
+    text-align: {title_text_align};
+}}
 
-    h1, h2, h3, h4, h5, h6 {{
-        color: #333; /* Adjust color if needed */
-    }}
+h1, h2, h3, h4, h5, h6 {{
+    color: #333; /* Adjust color if needed */
+}}
 
-    /* GitHub-like code highlighting */
-    pre {{
-        background-color: {code_background_color}; /* Darker background color for code */
-        border: 1px solid {code_border_color}; /* Border color */
-        border-radius: {code_border_radius}px;
-        overflow: auto;
-        padding: {code_padding}px;
-        max-width: {code_max_width}px; /* Set your desired maximum width */
-        margin: 0 auto; /* Center the code block */
-    }}
+/* GitHub-like code highlighting */
+pre {{
+    background-color: {code_background_color}; /* Darker background color for code */
+    border: 1px solid {code_border_color}; /* Border color */
+    border-radius: {code_border_radius}px;
+    overflow: auto;
+    padding: {code_padding}px;
+    max-width: {code_max_width}px; /* Set your desired maximum width */
+    margin: 0 auto; /* Center the code block */
+}}
 
-    /* Syntax highlighting colors for VSCode dark theme */
-    pre code {{
-        display: block;
-        font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
-        font-size: 14px;
-        color: {code_text_color}; /* Adjusted light text color for code */
-        span.va {{ color: {variable_color}; }}; /* Variable */
-        span.im {{ color: {import_color}; }}; /* Import */
-        span.fu {{ color: {function_color}; }} /* Function */
-    }}
-    """
+/* Syntax highlighting colors for VSCode dark theme */
+pre code {{
+    display: block;
+    font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+    font-size: 14px;
+    color: {code_text_color}; /* Adjusted light text color for code */
+    span.va {{ color: {variable_color}; }}; /* Variable */
+    span.im {{ color: {import_color}; }}; /* Import */
+    span.fu {{ color: {function_color}; }} /* Function */
+}}"""
 
     def save(self, filename: str):
         with open(filename, "w") as file:
